4d965f1e5e756d9953bb89c888b71bba3262ae09
Nikola04 <Nikola04@NIKOLA>
2022-12-22 21:36:52 +0200

    Add project files.

---
+++ ASP Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ASP_Project.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_Project.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public override DbSet<User> Users { get; set; }
+    }
+}
